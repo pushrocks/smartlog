27bb9a789c25e7844891ae654a0106dea44c68a0
Phil Kunz <gitlab@philkunz.com>
2020-06-11 13:23:07 +0000

    fix(core): update

--- ts/smartlog.classes.consolelog.ts
+++ ts/smartlog.classes.consolelog.ts
@@ -4,7 +4,12 @@ import * as plugins from './smartlog.plugins';
  * a console log optimized for smartlog
  */
 export class ConsoleLog {
-  public log(logLevelArg: plugins.smartlogInterfaces.TLogLevel, logMessageArg: string) {
+  public log(
+    logLevelArg: plugins.smartlogInterfaces.TLogLevel,
+    logMessageArg: string,
+    dataArg?: any,
+    correlationArg?: plugins.smartlogInterfaces.ILogCorrelation
+  ) {
     console.log(`__# ${logLevelArg}: ${logMessageArg}`);
   }
 }
